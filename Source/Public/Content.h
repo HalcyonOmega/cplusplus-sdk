030b5bd8382503118d8f1e8ee96367f651523be8
HalcyonOmega <129798039+HalcyonOmega@users.noreply.github.com>
2025-06-12 18:22:50 +0000

    Cleanup

--- Source/Public/Content.h
+++
@@ -1,6 +0,0 @@
-// *   `ContentBase`: Abstract base for rich content.
-//     *   `TextContent`
-//     *   `ImageContent`
-//     *   `AudioContent`
-//     *   `EmbeddedResourceContent` (references `TextResourceContents` or
-//     `BlobResourceContents`)
--- Source/Public/Initialize.h
+++
@@ -1,5 +0,0 @@
-// *   `InitializeParams`: (`ProtocolVersion`, `Capabilities`, `ClientInfo`)
-// *   `InitializeResult`: (`ProtocolVersion`, `Capabilities`, `ServerInfo`, `Instructions?`)
-// *   `InitializedParams`: (Parameters for the `initialized` notification. If the spec defines
-// no parameters for this notification, `void` would be used directly as `ParamsType` for
-// `Notification<ParamsType>` and this struct would not be needed.)
