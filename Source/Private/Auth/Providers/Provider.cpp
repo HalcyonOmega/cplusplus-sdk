8df8a83007a20cbc05bb17233bad5f39e42058ac
HalcyonOmega <129798039+HalcyonOmega@users.noreply.github.com>
2025-06-19 12:55:16 +0000

    Rebaseline coresdk

--- Source/Private/Auth/Providers/Provider.cpp
+++
@@ -1,5 +0,0 @@
-#include "Auth/Providers/Provider.h"
-
-MCP_NAMESPACE_BEGIN
-
-MCP_NAMESPACE_END
--- Source/Private/Communication/Transport/InMemory/InMemory.cpp
+++
@@ -1,5 +0,0 @@
-#include "Communication/Transport/InMemory/InMemory.h"
-
-MCP_NAMESPACE_BEGIN
-
-MCP_NAMESPACE_END
